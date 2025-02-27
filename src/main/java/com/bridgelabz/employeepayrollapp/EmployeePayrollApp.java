59441e9ca6f524df74fadb044aa884aab8b936ac
av1401 <rsabhi1401@gmail.com>
2025-02-27 15:39:07 +0530

    [Abhishek] Added section 1 UC 1

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApp.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollApp.class, args);
+		System.out.println("Employee Payroll App is running...");
+	}
+}
